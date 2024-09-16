ee370352359edbc23390a4e17dafbc3bebfd9a03
Kudl1k <stepanek.kudlacek@gmail.com>
2024-09-16 13:38:29 +0200

    Created a charger details page.
    Created a charger details page, which is displaying some basic information about the charger. Later there will be displayed more information

--- web/src/components/cards/ChargersCard.tsx
+++ web/src/components/cards/ChargersCard.tsx
@@ -1,41 +1,63 @@
 import { Charger } from "@/models/ChargerData";
-import { Button } from "../ui/button";
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "../ui/card";
+import { Atom, Cable, Ruler } from "lucide-react";
+import { Link } from "react-router-dom";
 import { Badge } from "../ui/badge";
-
-
+import { buttonVariants } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 
 interface ChargersCardProps {
-    charger: Charger
+  charger: Charger;
 }
 
-export function ChargersCard({charger}: ChargersCardProps) {
-
-    return (
-        <Card className="shadow-md">
-            <CardHeader>
-                <CardTitle>{charger.name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <div className="pb-3">
-                    <span className="font-semibold">Types: </span>
-                    {charger.types.map((type) => (
-                        <Badge className="ms-1" key={type.shortcut} variant="outline">{type.shortcut}</Badge>
-                    ))}
-                </div>
-                <div>
-                    <span className="font-semibold">Sizes:</span>
-                    {charger.sizes.map((size) => (
-                        <Badge className="ms-1" key={size.name} variant="outline">{size.name}</Badge>
-                    ))}
-                </div>
-            </CardContent>
-            <CardFooter>
-                <div className="flex justify-between w-full">
-                    <Button variant={"secondary"} >Details</Button>
-                </div>
-            </CardFooter>
-        </Card>
-    )
-
+export function ChargersCard({ charger }: ChargersCardProps) {
+  return (
+    <Card className="shadow-md">
+      <CardHeader>
+        <div className="flex w-full space-x-2">
+          <Cable />
+          <CardTitle>
+            <h2 className="text-xl">{charger.name}</h2>
+          </CardTitle>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div className="pb-3 flex">
+          <span className="font-semibold">
+            <Atom />
+          </span>
+          {charger.types.map((type) => (
+            <Badge className="ms-1" key={type.shortcut} variant="outline">
+              {type.shortcut}
+            </Badge>
+          ))}
+        </div>
+        <div className="flex">
+          <span className="font-semibold">
+            <Ruler />
+          </span>
+          {charger.sizes.map((size) => (
+            <Badge className="ms-1" key={size.name} variant="outline">
+              {size.name}
+            </Badge>
+          ))}
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className="flex justify-between w-full">
+          <Link
+            className={buttonVariants({ variant: "secondary" })}
+            to={`/chargers/${charger.id}`}
+          >
+            Details
+          </Link>
+        </div>
+      </CardFooter>
+    </Card>
+  );
 }
--- web/src/pages/chargers/ChargersPage.tsx
+++ web/src/pages/chargers/ChargersPage.tsx
@@ -8,75 +8,79 @@ import { PlugZap } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 export function ChargersPage() {
-    const [chargers, setChargers] = useState<Charger[] | null>(null);
-    const [page, setPage] = useState(1);
-    const [perPage] = useState(9);
-    const hasFetched = useRef(false);
+  const [chargers, setChargers] = useState<Charger[] | null>(null);
+  const [page, setPage] = useState(1);
+  const [perPage] = useState(9);
+  const hasFetched = useRef(false);
 
+  useEffect(() => {
+    if (!hasFetched.current) {
+      fetchChargerData(setChargers);
 
-    useEffect(() => {
-        if (!hasFetched.current) {
-            fetchChargerData(setChargers);
-
-            hasFetched.current = true;
-        }
-    }, []);
-
-    useEffect(() => {
-        const params = new URLSearchParams(location.search);
-        const pageParam = params.get('page');
-        if (pageParam) {
-            setPage(Number(pageParam));
-        }
-    }, []);
-
-
-    if (!chargers) {
-        return Loading();
+      hasFetched.current = true;
     }
+  }, []);
 
-    console.log(chargers);
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const pageParam = params.get("page");
+    if (pageParam) {
+      setPage(Number(pageParam));
+    }
+  }, []);
 
-    const start = (Number(page) - 1) * Number(perPage)
-    const end = start + Number(perPage)
+  if (!chargers) {
+    return Loading();
+  }
 
-    const paginatedData = chargers.slice(start, end)
+  console.log(chargers);
 
+  const start = (Number(page) - 1) * Number(perPage);
+  const end = start + Number(perPage);
 
-    return (
-        <>
-            <div className="flex justify-center">
-                <div className="flex justify-end w-full xl:w-4/6 ps-4 pe-4 pt-4">
-                <div>
-                    <Link className={buttonVariants({ variant: "default" })} to={"/add?tab=charger"} >Add new charger</Link>
-                </div>
-                </div>
-            </div>
-            {chargers.length === 0 && 
-                <div className="flex justify-center">
-                    <div className="w-full xl:w-1/4 lg:w-3/6 md:w-4/6 ps-4 pe-4 pt-4" >
-                        <InfoBox
-                            alertTitle="No batteries found"
-                            alertDescription="There are no batteries found in the database. Please add a new battery."
-                            icon={PlugZap}
-                        />
-                    </div>
-                </div>  
-            }
-            <div className="flex justify-center">
-                <div className="w-full xl:w-4/6 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-                    {paginatedData.map((charger) => (
-                        <ChargersCard key={charger.id} charger={charger}/>    
-                    ))}
-                </div>
-            </div>
-            {chargers.length > 0 && 
-                <PaggingMenu totalPosts={chargers.length} postsPerPage={perPage} currentPage={page} setCurrentPage={setPage} />  
-            }
-        </>
-    )
+  const paginatedData = chargers.slice(start, end);
 
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="flex justify-end w-full xl:w-4/6 ps-4 pe-4 pt-4">
+          <div>
+            <Link
+              className={buttonVariants({ variant: "default" })}
+              to={"/add?tab=charger"}
+            >
+              Add new charger
+            </Link>
+          </div>
+        </div>
+      </div>
+      {chargers.length === 0 && (
+        <div className="flex justify-center">
+          <div className="w-full xl:w-1/4 lg:w-3/6 md:w-4/6 ps-4 pe-4 pt-4">
+            <InfoBox
+              alertTitle="No batteries found"
+              alertDescription="There are no batteries found in the database. Please add a new battery."
+              icon={PlugZap}
+            />
+          </div>
+        </div>
+      )}
+      <div className="flex justify-center">
+        <div className="w-full xl:w-4/6 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 gap-4 p-4">
+          {paginatedData.map((charger) => (
+            <ChargersCard key={charger.id} charger={charger} />
+          ))}
+        </div>
+      </div>
+      {chargers.length > 0 && (
+        <PaggingMenu
+          totalPosts={chargers.length}
+          postsPerPage={perPage}
+          currentPage={page}
+          setCurrentPage={setPage}
+        />
+      )}
+    </>
+  );
 }
